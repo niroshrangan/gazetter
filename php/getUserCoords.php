357f8426e41b986782909f71c2b44658c62a4c38
niroshrangan <59030729+niroshrangan@users.noreply.github.com>
2021-06-09 16:46:59 +0100

    Update getUserCoords.php

--- php/getUserCoords.php
+++ php/getUserCoords.php
@@ -1,8 +1,29 @@
 <?php
 
-$lat = $_GET['lat'];
-$lng = $_GET['lng'];
-$username = $_GET['username'];
-// The file_get_contents() reads a file into a string.
-$data = file_get_contents("http://api.geonames.org/countryCodeJSON?lat=$lat&lng=$lng&username=$username");
-print_r($data);
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    $executionStartTime = microtime(true);
+
+    $url = "http://api.geonames.org/countryCodeJSON?lat=" . $_REQUEST['lat'] . "&lng=" . $_REQUEST['lng'] . "&username=n1509";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
+
+    $result=curl_exec($ch);
+
+    curl_close($ch);
+
+    $decode = json_decode($result,true);
+
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+
+
+    echo($result); // may have to change echo to print_r()
+
+?>
