1bad0c90544e6520055cf91360ab798e5cd24149
niroshrangan <59030729+niroshrangan@users.noreply.github.com>
2021-06-09 16:45:49 +0100

    Update getCountryCode.php

--- php/getCountryCode.php
+++ php/getCountryCode.php
@@ -1,20 +1,24 @@
 <?php
 
-$string = file_get_contents("../data/countryBorders.geo.json");
-$json = json_decode($string);
-$features = $json->features;
+    $fileContents = file_get_contents("../data/countryBorders.geo.json");
+    $output = json_decode($fileContents);
+    $features = $output->features;
 
-$all_countries = array();
-for($i=0;$i<sizeof($features);$i++){
-    $feature = $features[$i];
-    $country_name = $feature->properties->name;
-    $country_iso_a2 = $feature->properties->iso_a2;
-    $array = [$country_name,$country_iso_a2];
-    array_push($all_countries, $array);
-}
-usort($all_countries, function($a, $b) {
-    return strcasecmp($a[0], $b[0]);
-});
-print_r(json_encode($all_countries));
+    $countryData = [];
+    $arrayElement = [];
 
-//print_r($json);
+    foreach ($features as $element) {
+        $countryName = $element->properties->name;
+        $countryCode = $element->properties->iso_a2;
+
+        $arrayElement = [$countryName, $countryCode];
+        array_push($countryData, $arrayElement);
+    }
+
+    usort($countryData, function($country, $nextCountry) {
+        return strcmp($country[0], $nextCountry[0]);
+    });
+
+    echo json_encode($countryData);
+
+?>
